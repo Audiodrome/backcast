c297d1493b2dc8d91e95c7ee3f80486c141913ad
Hack Reactor Student <student@hackreactor.com>
2017-08-29 08:03:27 -0700

    Backcast has a rudimentry rendered view. Still does entirely nothing. Videos collections was instantiated with the mocked data.

--- src/views/app.js
+++ src/views/app.js
@@ -3,7 +3,9 @@ var AppView = Backbone.View.extend({
   el: '#app',
 
   initialize: function() {
-    this.videos = new Videos();
+    this.videos = new Videos(window.exampleVideoData);
+    console.log(this.videos);
+    this.render();
   },
 
 
